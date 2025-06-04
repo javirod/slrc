c713caf3df250763f34823d08e7a2c66d7a944f3
Javier Rodriguez <javirod09@gmail.com>
2025-06-04 13:52:07 -0700

    Init commit

---
+++ server/src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express()
+
+const port = process.env.PORT || 3001
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => console.log("SERVER STARTED"));
